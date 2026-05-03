208a124b736c2da7f9b61e82ba517ee0bb934fe6
iamkaran <kforkaranveer@gmail.com>
2026-05-03 21:45:59 +0530

    clangd lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -90,3 +90,11 @@ vim.lsp.config(
 vim.lsp.enable(
     "gopls"
 )
+
+vim.lsp.config("clangd", {
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    root_markers = { ".clangd", "compile_commands.json", "compile_flags.txt", "CMakeLists.txt", ".git" },
+})
+
+vim.lsp.enable("clangd")
